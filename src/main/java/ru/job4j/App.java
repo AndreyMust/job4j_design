7f0139ea4bd7d3f86bc76677bc5e73d9bab68a36
Andrew <andreymustafin.it@gmail.com>
2022-05-02 21:01:15 +0300

    add App.java

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -1,5 +1,7 @@
 package ru.job4j;
 
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
@@ -7,5 +9,17 @@ package ru.job4j;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        int[][] in = {{1}, {2, 3}};
+
+        for (int[] row: in) {
+            for (int el: row) {
+                //System.out.println(row.length);
+                //System.out.println(el);
+                arrayList.add(el);
+            }
+        }
+        System.out.println(arrayList);
     }
 }
