eaa4681323b58a0155194015bfe6851f9ec58e27
Andrew <andreymustafin.it@gmail.com>
2022-05-04 13:12:50 +0300

    2. Группировка и агрегатные функции [#373618 #296460] fix1

--- src/main/java/ru/job4j/sql/aggregate.sql
+++ src/main/java/ru/job4j/sql/aggregate.sql
@@ -7,11 +7,12 @@ create table devices(
 insert into devices(name, price)
 values('HDD', 6500), ('RAM_4GB', 4200), ('Keyboard', 1400),
 ('RAM_16GB', 12200), ('GPU', 54000)
+;
 
 insert into devices(name, price)
 values('HDD', 6700), ('RAM_4GB', 4300), ('Keyboard', 1550),
 ('RAM_16GB', 33500), ('GPU', 62100)
-
+;
 
 create table people(
     id serial primary key,
@@ -20,21 +21,25 @@ create table people(
 
 insert into people(name)
 values ('Anna'), ('Ivan'), ('Petr'), ('Albert')
+;
 
 select * from people
+;
 select * from devices
+;
 
 --средняя стоимость устройств
 select name, avg(price)
 from devices
 group by name
-
+;
 
 --средняя стоимость устройств больше 5000
 select name, avg(price)
 from devices
 group by name
 having avg(price)>5000
+;
 
 create table devices_people(
     id serial primary key,
@@ -47,6 +52,7 @@ values (1,1), (1,2), (1,3), (1,4),
 (2,1), (2,2), (2,3),
 (6,1), (7,2), (8,3), (9,4),
 (4,1), (3,2), (2,3), (8,4)
+;
 
 --средняя стоимость всех устройств у пользователя
 select p.name, avg(d.price)
@@ -54,4 +60,5 @@ from people as p
 join devices_people dp on dp.people_id = p.id
 join devices d on d.id = dp.device_id
 group by p.name
+;
 
