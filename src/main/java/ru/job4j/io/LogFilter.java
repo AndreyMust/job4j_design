ff0ab65d8647b114f2ab4de7c8918ba139aaaf57
Andrew <andreymustafin.it@gmail.com>
2022-05-13 20:00:58 +0300

    0.3. BufferedReader. [#252489]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LogFilter {
+
+    /*Метод filter должен прочитать файл и вернуть строки, где ПРЕДпоследнее значение - равно 404.*/
+    public List<String> filter(String file) {
+        List<String> log = new ArrayList<>();
+
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            log = in.lines()
+                    .filter(e -> "404".equals(e.split(" ")[e.split(" ").length - 2]))
+                    .collect(Collectors.toList());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return log;
+    }
+
+    public static void main(String[] args) {
+        LogFilter logFilter = new LogFilter();
+        List<String> log = logFilter.filter("./data/log.txt");
+        log.forEach(e -> System.out.println(e));
+    }
+}
