f82b47eb83c25022fbb85e135e1a6efd1a519f6d
Andrew <andreymustafin.it@gmail.com>
2022-05-13 19:33:50 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+import java.util.Scanner;
+
+/* Вывод таблицы умножения в файл*/
+public class ResultFile {
+
+    public static void main(String[] args) {
+        System.out.println("Enter the number:");
+        Scanner input = new Scanner(System.in);
+        int size = Integer.parseInt(input.nextLine());
+        try (FileOutputStream out = new FileOutputStream("./data/result.txt")) {
+            for (int row = 1; row <= size; row++) {
+                for (int col = 1; col <= size; col++) {
+                    int mult = row * col;
+                    String space = mult < 10 ? "  " : " ";
+                    out.write((mult + space).getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
