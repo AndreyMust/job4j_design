a0ef3ec0506b70a941c661fd78ed0e7e8a267506
Andrew <andreymustafin.it@gmail.com>
2022-05-20 15:30:43 +0300

    3. Формат XML [#313165]

---
+++ src/main/java/ru/job4j/serialization/xml/Book.java
@@ -0,0 +1,9 @@
+package ru.job4j.serialization.xml;
+
+public class Book {
+    private String name;
+    private int listCount;
+    private boolean isPaperBook;
+    private String[] authors;
+    private Contact contact;
+}
