094fe42bd90c68b7fb0f912a49470a3f72f0c8ac
Andrew <andreymustafin.it@gmail.com>
2022-08-08 22:01:39 +0300

    1. Принципы Kiss, Dry и Yagni [#238813]

---
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.kiss;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MaxMinTest {
+
+    @Test
+    public void testMax() {
+        List<Integer> integerList = Arrays.asList(0, 1, 2, 3);
+        MaxMin maxMin = new MaxMin();
+        Integer max = maxMin.max(integerList, (o1, o2) -> o2 - o1);
+        Assert.assertEquals(3, (int) max);
+
+    }
+
+    @Test
+    public void testMin() {
+        List<Integer> integerList = Arrays.asList(0, 1, 2, 3);
+        MaxMin maxMin = new MaxMin();
+        Integer min = maxMin.min(integerList, (o1, o2) -> o2 - o1);
+        Assert.assertEquals(0, (int) min);
+    }
+}
