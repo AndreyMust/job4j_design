b0c515bc02d2bb61501d6df792a1261e28a40fb1
Andrew <andreymustafin.it@gmail.com>
2023-01-12 01:17:35 +0300

    2.1. Преобразование даты [#289476 #379829]  fix1

--- src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.grabber.utils;
 
 import org.junit.jupiter.api.Test;
+
 import java.time.LocalDateTime;
 import static org.assertj.core.api.Assertions.*;
 
@@ -27,4 +28,10 @@ class HabrCareerDateTimeParserTest {
         assertThat(parsed).isBefore("2024-01-01T00:00:01")
                 .isAfter("2020-01-01T00:30:00");
     }
+
+    /*
+    *
+        Примеры работы с assertj
+        https://github.com/assertj/assertj-examples/blob/main/assertions-examples/src/test/java/org/assertj/examples/Java8DateTimeAssertionsExamples.java
+     */
 }
