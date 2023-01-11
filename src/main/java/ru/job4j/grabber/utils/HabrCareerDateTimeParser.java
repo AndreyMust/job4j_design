29929961c21facb0efe4f27a0817552c955f4179
Andrew <andreymustafin.it@gmail.com>
2023-01-12 01:02:44 +0300

    2.1. Преобразование даты [#289476 #379829] fix

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -5,14 +5,18 @@ import java.time.format.DateTimeFormatter;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
 
+    private static final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ISO_DATE_TIME;
+
     @Override
     public LocalDateTime parse(String parse) {
-        DateTimeFormatter dtf = DateTimeFormatter.ISO_DATE_TIME;
-        return LocalDateTime.parse(parse, dtf);
+        return LocalDateTime.parse(parse, DATE_TIME_FORMAT);
     }
 
     public static void main(String[] args) {
         DateTimeParser dateTimeParser = new HabrCareerDateTimeParser();
+        LocalDateTime asserted =  LocalDateTime.parse("2023-01-10T15:13:38", DateTimeFormatter.ISO_LOCAL_DATE_TIME);
         System.out.println(dateTimeParser.parse("2023-01-10T15:13:38+03:00"));
+        System.out.println(asserted);
+
     }
 }
---
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.grabber.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import static org.assertj.core.api.Assertions.*;
+
+class HabrCareerDateTimeParserTest {
+
+    @Test
+    public void whenParseDateTimeOne() {
+        DateTimeParser dateTimeParser = new HabrCareerDateTimeParser();
+        LocalDateTime parsed = dateTimeParser.parse("2023-01-10T15:13:38+03:00");
+        assertThat(parsed).isEqualTo("2023-01-10T15:13:38");
+    }
+
+    @Test
+    public void whenParseDateTimeTwo() {
+        DateTimeParser dateTimeParser = new HabrCareerDateTimeParser();
+        LocalDateTime parsed = dateTimeParser.parse("2023-01-05T11:33:38+03:00");
+        assertThat(parsed).isEqualTo("2023-01-05T11:33:38");
+    }
+
+    @Test
+    public void whenParseDateTimeThree() {
+        DateTimeParser dateTimeParser = new HabrCareerDateTimeParser();
+        LocalDateTime parsed = dateTimeParser.parse("2023-01-05T11:33:38+03:00");
+        assertThat(parsed).isBefore("2024-01-01T00:00:01")
+                .isAfter("2020-01-01T00:30:00");
+    }
+
+    /*
+        https://github.com/assertj/assertj-examples/blob/main/assertions-examples/src/test/java/org/assertj/examples/Java8DateTimeAssertionsExamples.java
+     */
+}
