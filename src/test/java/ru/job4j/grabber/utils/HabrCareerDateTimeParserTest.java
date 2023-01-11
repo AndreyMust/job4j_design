4b0997bc91672d80d4f9909b26e093a6341bfa3c
Andrew <andreymustafin.it@gmail.com>
2023-01-12 02:28:01 +0300

    2.2. Модель данных - Post. [#285211] fix

--- src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -28,10 +28,4 @@ class HabrCareerDateTimeParserTest {
         assertThat(parsed).isBefore("2024-01-01T00:00:01")
                 .isAfter("2020-01-01T00:30:00");
     }
-
-    /*
-    *
-        Примеры работы с assertj
-        https://github.com/assertj/assertj-examples/blob/main/assertions-examples/src/test/java/org/assertj/examples/Java8DateTimeAssertionsExamples.java
-     */
 }
