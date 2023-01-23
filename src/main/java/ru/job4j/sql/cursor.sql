5e508be389ab2fd63645b7f6f87cf763a74c2619
Andrew <andreymustafin.it@gmail.com>
2023-01-23 12:12:24 +0300

    5. Курсор в SQL [#504816 #379818] fix1

--- src/main/java/ru/job4j/sql/cursor.sql
+++ src/main/java/ru/job4j/sql/cursor.sql
@@ -14,6 +14,8 @@ FETCH FROM cursor_products;
 MOVE BACKWARD 2 FROM cursor_products;
 FETCH FROM cursor_products;
 FETCH FIRST FROM cursor_products;
-rollback;
 
 CLOSE cursor_products;
+DEALLOCATE cursor_products;
+
+rollback;
